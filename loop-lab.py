8cd9cc8582f1f7122daf21dc527768945fc427d7
M7mdQash <sil.qash@protonmail.com>
2026-02-18 13:41:43 +0300

    solved lab and bonus lab

---
+++ loop-lab.py
@@ -0,0 +1,28 @@
+'''
+Docstring for bootcamp-tasks.d4-2ndtask.LAB_LOOPS.loop-lab
+# LAB_LOOPS
+
+
+## 1) Using range(),  make a range from 45 to 210, using a for loop iterate over the sequence and print the elements. Skip the number 100 and break the loop at 205
+
+## 2) Using a while loop and input , do the following :
+- Ask the the user : "what is the product of 7 * 24 ?"
+- check if the answer is right then exit the loop and print "You answered this Question correctly"
+- if the answer is wrong, then print "Your Answer is wrong try again.." and show the user the question again.
+
+
+'''
+
+for i in range (45,250):
+    if i == 100:
+        continue
+    elif i == 205:
+        break
+    print(f"element currently at: {i}")
+    
+while int(input("what is 7 * 24? ")) != 168:
+    print("Your Answer is wrong try again..\n")
+print("You answered this Question correctly")
+
+
+
