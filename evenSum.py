f7db4745dbc718f1fc76aae84df488bfa657af6a
M7mdQash <sil.qash@protonmail.com>
2026-02-18 13:43:35 +0300

    removed refrence clutter

--- evenSum.py
+++ evenSum.py
@@ -1,21 +1,3 @@
-'''
-## Bonus
-
-### Sum of Even Numbers
-
-Write a Python program that prompts the user for a positive integer `n`, and then calculates the sum of all even numbers between 1 and `n`, inclusive.
-
-Your program should use a loop (either a `for` loop or a `while` loop) to iterate over the numbers between 1 and `n`, and only add the even numbers to the sum.
-
-For example:
-
-```
-Enter a positive integer: 10
-The sum of even numbers between 1 and 10 is 30.
-```
-
-In this example, the even numbers between 1 and 10 are 2, 4, 6, 8, and 10, and their sum is 30.
-'''
 
 n =int( input("please provide a number:"))
 sum = 0
